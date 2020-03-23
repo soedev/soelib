135824fecdcad3a1e5e93e7e364f3ae3c2fe8f56
wzx <1019868356@qq.com>
2020-03-23 09:47:23 +0800

    fix:去除sql日志

--- common/db/tenantdb/tenant_db.go
+++ common/db/tenantdb/tenant_db.go
@@ -41,7 +41,7 @@ func GetSQLDb(tenantID string, crmdb *gorm.DB) (*gorm.DB, error) {
 	sqlDb.DB().SetMaxIdleConns(1)                  //最大空闲数
 	sqlDb.DB().SetMaxOpenConns(5)                  //最大连接数
 	sqlDb.DB().SetConnMaxLifetime(time.Minute * 5) //设置最大空闲时间，超过将关闭连接
-	sqlDb.LogMode(true)
+	sqlDb.LogMode(false)
 	return sqlDb, nil
 }
 
