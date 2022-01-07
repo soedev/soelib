8636a519490eb66e46e36f31d8219b63499e07ac
何涛 <soeht@soesoft.com>
2022-01-07 13:53:08 +0800

    fix:慢接口统计新增访问记录

--- net/slowInterface/slow_interface.go
+++ net/slowInterface/slow_interface.go
@@ -3,8 +3,10 @@ package slowInterface
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/soedev/soelib/net/kafka"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -18,6 +20,10 @@ type SlowInterfaceParams struct {
 	Tag       string                 `json:"tag"`
 	TenantID  string                 `json:"tenantId"`
 	ShopCode  string                 `json:"shopCode"`
+	Type      int                    `json:"type"`
+	Datetime  time.Time              `json:"datetime"`
+	IsSkip    bool                   `json:"isSkip"`
+	Key       string                 `json:"key"`
 }
 
 //SlowInterface 慢接口统计
@@ -26,57 +32,65 @@ func SlowInterface(kafkaServer, tag string, slowTime int) gin.HandlerFunc {
 }
 
 func GetSlowInterface(kafkaServer, tag string, slowTime int) gin.HandlerFunc {
-	notlogged := make([]string, 0)
-
-	var skip map[string]struct{}
-
-	if length := len(notlogged); length > 0 {
-		skip = make(map[string]struct{}, length)
-
-		for _, path := range notlogged {
-			skip[path] = struct{}{}
-		}
-	}
+	// 选择要跳过的接口
+	//notlogged := make([]string, 0)
+	//
+	//var skip map[string]struct{}
+	//
+	//if length := len(notlogged); length > 0 {
+	//	skip = make(map[string]struct{}, length)
+	//
+	//	for _, path := range notlogged {
+	//		skip[path] = struct{}{}
+	//	}
+	//}
 
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
 
+		// Log only when path is not being skipped
+		//if _, ok := skip[path]; !ok {
+		tenantID := c.Request.Header.Get("tenantId")
+		shopCode := c.Request.Header.Get("shopCode")
+		if tenantID == "" || shopCode == "" {
+			return
+		}
+
+		param := SlowInterfaceParams{
+			Key:      strings.ReplaceAll(uuid.New().String(), "-", ""),
+			Request:  c.Request,
+			Keys:     c.Keys,
+			Tag:      tag,
+			TenantID: tenantID,
+			ShopCode: shopCode,
+			Datetime: start,
+			Path:     path,
+			Method:   c.Request.Method,
+			Type:     1,
+		}
+
+		bytes, _ := json.Marshal(param)
+		// 发送到kafka，记录访问时间
+		kafka.SendSarama(kafkaServer, "slow_interface", bytes)
+
 		// Process request
 		c.Next()
 
-		// Log only when path is not being skipped
-		if _, ok := skip[path]; !ok {
-			tenantID := c.Request.Header.Get("tenantId")
-			shopCode := c.Request.Header.Get("shopCode")
-			if tenantID == "" || shopCode == "" {
-				return
-			}
+		// 计算访问时间
+		param.TimeStamp = time.Now()
+		param.Latency = param.TimeStamp.Sub(start)
 
-			param := SlowInterfaceParams{
-				Request:  c.Request,
-				Keys:     c.Keys,
-				Tag:      tag,
-				TenantID: tenantID,
-				ShopCode: shopCode,
-			}
-
-			// Stop timer
-			param.TimeStamp = time.Now()
-			param.Latency = param.TimeStamp.Sub(start)
-
-			times := slowTime * 1000000
-			if param.Latency < time.Duration(times) {
-				return
-			}
-
-			param.Method = c.Request.Method
-
-			param.Path = path
-
-			bytes, _ := json.Marshal(param)
-			// 发送到kafka
-			kafka.SendSarama(kafkaServer, "slow_interface", bytes)
+		times := slowTime * 1000000
+		if param.Latency < time.Duration(times) {
+			param.IsSkip = true
 		}
+		param.Type = 0
+		param.Datetime = param.TimeStamp
+
+		bytes, _ = json.Marshal(param)
+		// 发送到kafka，记录返回时间
+		kafka.SendSarama(kafkaServer, "slow_interface", bytes)
+		//}
 	}
 }
